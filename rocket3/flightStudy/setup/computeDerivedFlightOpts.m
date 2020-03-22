de4a736233fdd8a5930e48d16baae862cfe3c59d
Alexander Polidar <alexander.polidar@t-online.de>
2020-03-22 21:11:40 +0100

    Adjusted material properties, output materials. Also some changes in the structure of the code

--- rocket3/flightStudy/setup/computeDerivedFlightOpts.m
+++ rocket3/flightStudy/setup/computeDerivedFlightOpts.m
@@ -1,8 +1,17 @@
 function opts = computeDerivedFlightOpts(opts)
     ofRatio = 4;
     
-    tankPressure = 5e6;
-    combustionPressure = 4.5e6;
+    % Material properties from: https://www.nedal.com/wp-content/uploads/2016/11/Nedal-alloy-Datasheet-EN-AW-6082.pdf
+    opts.OxidizerTankPressure = 5e6;
+    opts.OxidizerTankDiameter = 0.075;
+    opts.OxidizerTankDensity  = 2700;
+    opts.OxidizerTankSigma  = 290*10e6/2;   % allowable stress: half of transile strength (assumption)
+    opts.OxidizerTankSafetyMargin = 1.5;
+    opts.ccCombustionPressure = 4.5e6;
+    opts.ccDiameter = 0.075;
+    opts.ccDensity  = 2700;
+    opts.ccSigma  = 290*10e6/2;             % allowable stress: half of transile strength (assumption)
+    opts.ccSafetyMargin = 1.5;
     
     opts.OxidizerMass = opts.PropellantMass * ofRatio / (ofRatio + 1);
     opts.FuelMass = opts.PropellantMass * (1) / (ofRatio + 1);
@@ -10,20 +19,26 @@ function opts = computeDerivedFlightOpts(opts)
     opts.OxidizerVolume = opts.OxidizerMass / 770;
     opts.FuelVolume = opts.FuelMass / 900;
     
-    % Define capsuleTank: capsuleTank(Volume, Diameter, Pressure, rhoOverSigma, Sigma, rho, safety margin);
-    [oxTankMass, oxTankLength, oxTankWallThickness] = capsuleTank(opts.OxidizerVolume, 0.075, tankPressure, 1.35e-5, 200*10^6, 2700, 1.5);
-    [ccMass, ccLength, ccWallThickness] = capsuleTank(opts.FuelVolume, 0.075, combustionPressure, 1.35e-5, 200*10^6, 2700, 1.5);
+    % Define capsuleTank: capsuleTank(Volume, Diameter, Pressure, Sigma, rho, safety margin);
+    [oxTankMass, oxTankLength, oxTankWallThickness, oxTankMassCheck, oxTankWallThicknessCheck] = capsuleTank(opts.OxidizerVolume, opts.OxidizerTankDiameter, opts.OxidizerTankPressure, opts.OxidizerTankSigma, opts.OxidizerTankDensity, opts.OxidizerTankSafetyMargin);
+    [ccMass, ccLength, ccWallThickness, ccMassCheck, ccWallThicknessCheck] = capsuleTank(opts.FuelVolume, opts.ccDiameter, opts.ccCombustionPressure, opts.ccSigma, opts.ccDensity, opts.ccSafetyMargin);
     
-    opts.OxidizerTankLength = oxTankLength;
-    opts.FuelGrainLength = ccLength;
+    opts.OxidizerTankMass = oxTankMass;
+    opts.OxidizerTankMassCheck = oxTankMassCheck;
+    opts.OxidizerTankLength = oxTankLength;    
     opts.OxidizerTankWallThickness = oxTankWallThickness;
+    opts.OxidizerTankWallThicknessCheck = oxTankWallThicknessCheck;
+    opts.FuelGrainLength = ccLength;
+    opts.ccWallThickness = ccWallThickness;
+    opts.ccWallThicknessCheck = ccWallThicknessCheck;    
    
     engineMass = 6;
     dryMass = opts.PayloadMass + oxTankMass + ccMass + engineMass;
     wetMass = opts.PropellantMass + dryMass;
     
-    opts.OxidizerTankMass = oxTankMass;
+    
     opts.CombustionChamberMassIsh = ccMass;
+    opts.CombustionChamberMassIshCheck = ccMassCheck;
     opts.EngineMass = engineMass;
     
     opts.WetMass = wetMass;
