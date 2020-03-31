ab25b6cc7d3463e586f656ab9be282800687f781
eweilow <eweilow@digitalkoppar.se>
2020-03-31 10:33:55 +0200

    Render the system in a plot

--- rocket3/flightStudy/main_singleSimulation.m
+++ rocket3/flightStudy/main_singleSimulation.m
@@ -35,4 +35,79 @@ odeOpts = odeset('Events', @flightModelEvents, 'RelTol', 1e-8, 'AbsTol', 1e-8);
 plotSingleSimulation(flightOpts, t, State);
 
 %%
-outputOpts(flightOpts)
+
+y = 0;
+
+clf
+r = flightOpts.CombustionChamberRadius;
+pr = flightOpts.FuelGrainPortRadius;
+t = flightOpts.FuelGrainMarginThickness;
+L = flightOpts.CombustionChamberLength;
+[X, Y] = renderCapsuleTank(2*r, L);
+hold on
+
+ir = r-t;
+
+subplot(1,2,1);
+hold on
+axis equal
+
+offset = r;
+fill([r ir ir r], [0 0 L L]+offset, 'r', 'HandleVisibility', 'off')
+fill(-[r ir ir r], [0 0 L L]+offset, 'r', 'DisplayName', 'Unusable fuel (margin)');
+fill([pr ir ir pr], [0 0 L L]+offset, 'g', 'HandleVisibility', 'off')
+fill(-[pr ir ir pr], [0 0 L L]+offset, 'g', 'DisplayName', 'Usable fuel');
+plot(X, Y+r,  'm', 'LineWidth', 2, 'DisplayName', 'Combustion Chamber');
+
+lims = ylim;
+ylim([lims(1)-r lims(2)+r])
+title("Combustion chamber (under assumptions)");
+
+legend('show')
+
+subplot(1,2,2);
+hold on
+axis equal
+
+fill([r ir ir r], [0 0 L L]+offset, 'r', 'HandleVisibility', 'off')
+fill(-[r ir ir r], [0 0 L L]+offset, 'r', 'DisplayName', 'Unusable fuel (margin)');
+fill([pr ir ir pr], [0 0 L L]+offset, 'g', 'HandleVisibility', 'off')
+fill(-[pr ir ir pr], [0 0 L L]+offset, 'g', 'DisplayName', 'Usable fuel');
+plot(X, Y+r,  'm', 'LineWidth', 2, 'DisplayName', 'Combustion Chamber');
+axis equal
+offset = L + 4*r;
+
+L = flightOpts.OxidizerTankLength;
+[X, Y] = renderCapsuleTank(2*r, L);
+fill(X, Y+offset, [0.3 0.3 0.7], 'DisplayName', 'Oxidizer');
+plot(X, Y+offset, 'r', 'LineWidth', 2, 'DisplayName', 'Oxidizer tank');
+
+lims = ylim;
+ylim([lims(1)-r lims(2)+r])
+title("Full system (under assumptions)");
+legend('show')
+
+function [X, Y] = renderCapsuleTank(d, L)
+    N = 50;
+    
+    X = zeros(2*N+1,1);
+    Y = zeros(2*N+1,1);
+    
+    r = d/2;
+    
+    i = 1;
+    for theta = linspace(0, pi, N)
+        X(i) = cos(theta)*r;
+        Y(i) = L + sin(theta)*r;
+        i = i+1;
+    end
+    
+    for theta = linspace(pi, 2*pi, N)
+        X(i) = cos(theta)*r;
+        Y(i) = sin(theta)*r;
+        i = i+1;
+    end
+    
+    X(end) = X(1);
+    Y(end) = Y(1);
+end
