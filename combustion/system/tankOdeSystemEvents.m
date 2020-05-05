382102a763a6caf187db395056c07317f3d0ce4b
eweilow <eweilow@digitalkoppar.se>
2020-05-05 09:40:08 +0200

    More terminal events

--- combustion/system/tankOdeSystemEvents.m
+++ combustion/system/tankOdeSystemEvents.m
@@ -3,9 +3,9 @@ function [value,isterminal,direction] = tankOdeSystemEvents(t, X)
     % X is of the form [oxidizerMass, fuelMass, portRadius, ccPressure]
 
     % First event is oxidizer mass is depleted
-    value = [min(X(end), X(end))];
+    value = [X(1), X(end)];
 
     % Terminate simulation when hitting the ground
-    isterminal = [1];
-    direction = [0];
+    isterminal = [1 1];
+    direction = [0 0];
 end
