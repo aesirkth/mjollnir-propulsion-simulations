ab858783b5f7d280c0612dfac194e65d0a342dde
eweilow <eweilow@digitalkoppar.se>
2020-05-06 19:11:37 +0200

    Fix events

--- combustion/system/tankOdeSystemEvents.m
+++ combustion/system/tankOdeSystemEvents.m
@@ -3,7 +3,7 @@ function [value,isterminal,direction] = tankOdeSystemEvents(t, X)
     % X is of the form [oxidizerMass, fuelMass, portRadius, ccPressure]
 
     % First event is oxidizer mass is depleted
-    value = [X(1), X(end)];
+    value = [min(X(1), X(end))];
 
     % Terminate simulation when hitting the ground
     isterminal = [1];
