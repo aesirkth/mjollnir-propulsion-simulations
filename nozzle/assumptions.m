ad2a251f469f5f09dbe0405355018d0d4e958d06
eweilow <eweilow@digitalkoppar.se>
2020-05-06 22:47:37 +0200

    Update nozzle assumptions

--- nozzle/assumptions.m
+++ nozzle/assumptions.m
@@ -1,10 +1,15 @@
 % This file is run in nozzleSimulationAssumptions.m
 % Do not run this file as it is.
 %
-% This file contains assumptions for the nozzle simulation of the Eyjafjallajökull hybrid rocket engine.
+% This file contains assumptions for the nozzle simulation of the Eyjafjallaj??kull hybrid rocket engine.
 
+%% oh no
+
+opts.NozzleDesignGamma = 1.2;
+opts.NozzleDesignMassFlow = 3.5;
+opts.NozzleDesignCStar = 1600;
+
+opts.NozzleDesignCombustionPressureInMpa = 3.5;
 opts.NozzleExpansionPressureInAtmospheres = 1;
-opts.NozzleThroatDiameterInCm = 3;
-opts.NozzleExhaustDiameterInCm = 10; % The pressure which the nozzle expands exhaust to
 
 opts.NozzleEfficiency = 0.9;
