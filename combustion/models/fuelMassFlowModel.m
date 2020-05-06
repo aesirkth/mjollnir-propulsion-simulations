add5034e029fc04d36b6caac6c3e8cebb26b1b49
MaximePerriquet <maximep83@live.fr>
2020-05-06 18:39:01 +0200

    Corrections for sinusoidal shape

--- combustion/models/fuelMassFlowModel.m
+++ combustion/models/fuelMassFlowModel.m
@@ -1,3 +1,7 @@
 function massFlow = fuelMassFlowModel(regressionRate,portRadius,opts)
-massFlow = opts.FuelGrainDensity * regressionRate * 2 * pi * portRadius * opts.FuelGrainLength;
+Rinit = opts.FuelGrainInitialPortRadius;
+a = opts.CombustionChamberSinusShapeAmplitude;
+C8 = opts.CombustionChamberInitialPerimeter;
+coeff = 1+(C8/(2*pi*Rinit) - 1)*exp(sqrt(6/a)*(Rinit-portRadius)*2/Rinit);
+massFlow = (opts.FuelGrainDensity * regressionRate * 2 * pi * portRadius * opts.FuelGrainLength)*coeff;
 end
