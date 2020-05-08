4cd8193ae47411265a6dd17218a00d148dd6ead4
eweilow <eweilow@digitalkoppar.se>
2020-05-08 16:28:50 +0200

    Update plotting

--- combustion/createPlots.m
+++ combustion/createPlots.m
@@ -7,17 +7,21 @@ if isempty(burnOutIndex)
 end
 burnOutIndex = burnOutIndex(1);
 
+TSTART = 0.1;
+TEND = t(burnOutIndex - 2);
+
 if tankModel ~= 2
 
-setupSubplots(4,3)
+setupSubplots(5,3)
 nextPlot('');
 
-plot(t,combustionState.regressionRate*1000)
-xlim([0 t(burnOutIndex)]);
+plot(t,combustionState.regressionRate*1000, 'LineWidth', 2)
+xlim([TSTART TEND]);
 xlabel('Time [s]')
 ylabel('Regression Rate [mm/s]')
 title('Regression Rate over time')
 grid('on')
+scaleLims(0.1);
 nextPlot([filepath filesep 'regressionRate'])
 
 plot(t,combustionState.portRadius*1000, 'HandleVisibility','off')
@@ -31,7 +35,7 @@ y2 = combustionState.opts.TotalFuelGrainRadius + combustionState.opts.FuelGrainC
 y3 = combustionState.opts.CombustionChamberRadius; % Wall outer radius
 fill([t(1) t(end) t(end) t(1)], [y1 y1 y2 y2]*1e3, '-', 'LineWidth', 2,  'DisplayName', 'Fuel grain casing')
 fill([t(1) t(end) t(end) t(1)], [y2 y2 y3 y3]*1e3, '-', 'LineWidth', 2,  'DisplayName', 'Combustion chamber')
-xlim([0 t(burnOutIndex)]);
+xlim([TSTART TEND]);
 ylim([0, y3*1e3])
 xlabel('Time [s]')
 ylabel('Port Radius [mm]')
@@ -52,11 +56,10 @@ nextPlot([filepath filesep 'portRadius'])
 % grid('on')
 % nextPlot([filepath filesep 'allPressure'])
 
-plot(t,combustionState.tankPressure./combustionState.ccPressure, 'DisplayName', 'Tank/cc pressure')
+%
+semilogy(t,combustionState.tankPressure./combustionState.ccPressure, 'LineWidth', 2, 'DisplayName', 'CC/exhaust pressure')
 hold on;
-plot(t,combustionState.ccPressure./combustionState.exhaustPressure, 'DisplayName', 'CC/exhaust pressure')
-hold 
-xlim([0 t(burnOutIndex)]);
+xlim([TSTART TEND]);
 xlabel('Time [s]')
 ylabel('Pressure ratio [1]')
 title('Pressure ratio over time')
@@ -65,12 +68,34 @@ scaleLims(0.1);
 grid('on')
 nextPlot([filepath filesep 'pressureRatio'])
 
-plot(t,combustionState.tankPressure/1e6', 'DisplayName', 'Tank pressure')
+plot(t,(combustionState.tankPressure - combustionState.ccPressure)/1e6, 'LineWidth', 2, 'DisplayName', 'Pressure diff - tank vs cc')
 hold on;
-plot(t,combustionState.ccPressure/1e6', 'DisplayName', 'CC pressure')
-plot(t,combustionState.exhaustPressure/1e6', 'DisplayName', 'Exhaust pressure')
-hold 
-xlim([0 t(burnOutIndex)]);
+xlim([TSTART TEND]);
+xlabel('Time [s]')
+ylabel('Pressure difference [MPa]')
+title('Pressure difference over time')
+legend('show', 'Location', 'best');
+scaleLims(0.1);
+grid('on')
+nextPlot([filepath filesep 'pressureDiff'])
+
+plot(t,combustionState.exhaustPressure/1e5', 'LineWidth', 2, 'DisplayName', 'Exhaust pressure')
+hold on;
+xlim([TSTART TEND]);
+xlabel('Time [s]')
+ylabel('Pressure [bar]')
+title('Exhaust pressure over time')
+legend('show', 'Location', 'best');
+scaleLims(0.1);
+grid('on')
+nextPlot([filepath filesep 'exhaustPressure'])
+
+plot(t,combustionState.tankPressure/1e6', 'LineWidth', 2, 'DisplayName', 'Tank pressure')
+hold on;
+plot(t,combustionState.ccPressure/1e6', 'LineWidth', 2, 'DisplayName', 'CC pressure')
+plot(t,combustionState.exhaustPressure/1e6', 'LineWidth', 2, 'DisplayName', 'Exhaust pressure')
+hold on;
+xlim([TSTART TEND]);
 xlabel('Time [s]')
 ylabel('Pressure [MPa]')
 title('Pressure over time')
@@ -79,9 +104,9 @@ scaleLims(0.1);
 grid('on')
 nextPlot([filepath filesep 'allPressure'])
 
-plot(t,combustionState.exhaustMach', 'DisplayName', 'Exhaust mach')
-hold 
-xlim([0 t(burnOutIndex)]);
+plot(t,combustionState.exhaustMach', 'LineWidth', 2, 'DisplayName', 'Exhaust mach')
+hold on;
+xlim([TSTART TEND]);
 xlabel('Time [s]')
 ylabel('Mach [1]')
 title('Mach over time')
@@ -90,11 +115,11 @@ scaleLims(0.1);
 grid('on')
 nextPlot([filepath filesep 'mach'])
 
-plot(t,combustionState.ccPressure/1e6', 'HandleVisibility','off')
+plot(t,combustionState.ccPressure/1e6', 'LineWidth', 2, 'HandleVisibility','off')
 hold on
 plot([t(1) t(end)], [combustionState.opts.ccCombustionPressure, combustionState.opts.ccCombustionPressure]/1e6, '--', 'LineWidth', 2, 'DisplayName', 'Design pressure')
 plot([t(1) t(end)], [combustionState.opts.ccCombustionPressure, combustionState.opts.ccCombustionPressure]*combustionState.opts.ccCombustionPressureSafetyMargin/1e6, '--', 'LineWidth', 2, 'DisplayName', 'Design pressure (incl. safety)')
-xlim([0 t(burnOutIndex)]);
+xlim([TSTART TEND]);
 xlabel('Time [s]')
 ylabel('CC Pressure [MPa]')
 title('Combustion Chamber pressure over time')
@@ -104,65 +129,69 @@ grid('on')
 nextPlot([filepath filesep 'combustionPressure'])
 
 OF = combustionState.oxidizerMassFlow ./ combustionState.fuelMassFlow;
-plot(t,OF)
-xlim([0 t(burnOutIndex)]);
-ylim([0 max(OF)]);
+plot(t,OF, 'LineWidth', 2)
+xlim([TSTART TEND]);
 xlabel('Time [s]')
 ylabel('O/F ratio [MPa]')
 title('O/F ratio over time')
 grid('on')
+scaleLims(0.1);
 nextPlot([filepath filesep 'ofRatio'])
 
-plot(t,combustionState.thrust/1000, 'DisplayName', 'Real thrust (incl. efficiency)')
+plot(t,combustionState.thrust/1000, 'DisplayName', 'Real thrust', 'LineWidth', 2)
 hold on
-plot(t,combustionState.idealThrust/1000, 'DisplayName', 'Ideal thrust')
-xlim([0 t(burnOutIndex)]);
+plot(t,combustionState.idealThrust/1000, 'DisplayName', 'Ideal thrust', 'LineWidth', 2)
+xlim([TSTART TEND]);
 xlabel('Time [s]')
 ylabel('Thrust [kN]')
 title('Thrust over time')
 legend('show', 'Location', 'best');
 grid('on')
+scaleLims(0.1);
 nextPlot([filepath filesep 'thrust'])
 
-plot(t,combustionState.ccTemperature)
-xlim([0 t(burnOutIndex)]);
+plot(t,combustionState.ccTemperature, 'LineWidth', 2)
+xlim([TSTART TEND]);
 xlabel('Time [s]')
 ylabel('CC Temperature [K]')
 title('Combustion Chamber Temperature over time')
 grid('on')
+scaleLims(0.1);
 nextPlot([filepath filesep 'combustionTemperature'])
 
 plot(t,combustionState.fuelMass, 'LineWidth', 2, 'DisplayName', 'Fuel')
 hold on
 plot(t,combustionState.oxidizerMass, 'LineWidth', 2, 'DisplayName', 'Oxidizer')
 plot(t,combustionState.fuelMass + combustionState.oxidizerMass, 'LineWidth', 2, 'DisplayName', 'Total')
-xlim([0 t(burnOutIndex)]);
+xlim([TSTART TEND]);
 xlabel('Time [s]')
 ylabel('Mass [kg]')
 title('Propellant mass over time')
 legend('show', 'Location', 'best');
 grid('on')
+scaleLims(0.1);
 nextPlot([filepath filesep 'mass'])
 
 plot(t,combustionState.fuelMassFlow, 'LineWidth', 2, 'DisplayName', 'Fuel')
 hold on
 plot(t,combustionState.oxidizerMassFlow, 'LineWidth', 2, 'DisplayName', 'Oxidizer')
 plot(t,combustionState.fuelMassFlow + combustionState.oxidizerMassFlow, 'LineWidth', 2, 'DisplayName', 'Total')
-xlim([0 t(burnOutIndex)]);
+xlim([TSTART TEND]);
 xlabel('Time [s]')
 ylabel('Mass flow [kg/s]')
 title('Mass flow over time')
 legend('show', 'Location', 'best');
 grid('on')
+scaleLims(0.1);
 nextPlot([filepath filesep 'combustion'])
 
 end
 
-plot(t,combustionState.tankPressure/1e6, 'HandleVisibility','off')
+plot(t,combustionState.tankPressure/1e6, 'LineWidth', 2, 'HandleVisibility','off')
 hold on;
 plot([t(1) t(end)], [combustionState.opts.OxidizerTankPressure, combustionState.opts.OxidizerTankPressure]/1e6, '--', 'LineWidth', 2, 'DisplayName', 'Design pressure')
 plot([t(1) t(end)], [combustionState.opts.OxidizerTankPressure, combustionState.opts.OxidizerTankPressure]*combustionState.opts.OxidizerTankSafetyMargin/1e6, '--', 'LineWidth', 2, 'DisplayName', 'Design pressure (incl. safety)')
-xlim([0 t(burnOutIndex)]);
+xlim([TSTART TEND]);
 xlabel('Time [s]')
 ylabel('Oxidizer tank pressure [MPa]')
 title('Oxidizer tank pressure over time')
@@ -177,7 +206,7 @@ if tankModel ~= 0
     plot([t(1) t(end)], [combustionState.opts.AmbientTemperature, combustionState.opts.AmbientTemperature], '--', 'LineWidth', 2, 'DisplayName', 'Ambient temperature')
     plot(t,combustionState.wallTemperatureGNode, '--', 'LineWidth', 2, 'DisplayName','Wall temperature of gas node')
     plot(t,combustionState.wallTemperatureLNode, '--', 'LineWidth', 2, 'DisplayName','Wall temperature of liquid node')
-    xlim([0 t(burnOutIndex)]);
+    xlim([TSTART TEND]);
     xlabel('Time [s]')
     ylabel('Temperature temperature [T]')
     title('Oxidizer temperature over time')
